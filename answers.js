e02c20e771ac094ef61cf235860a2ce9607d97b6
Dhrumil Shah <36997383+DhrumilShah98@users.noreply.github.com>
2022-06-05 20:42:20 -0300

    Update answers.js

--- answers.js
+++ answers.js
@@ -32,27 +32,36 @@ const characters = [
     },
 ];
 
-//***MAP***
+//***********
+//*** MAP ***
+//***********
 //1. Get array of all names
 const names = characters.map(function(character){ return character.name; });
 console.log(names);
+
 //2. Get array of all heights
 const heights = characters.map(function(character){ return character.height; });
 console.log(heights);
+
 //3. Get array of objects with just name and height properties
 const namesHeights = characters.map(function(character){ return {name: character.name, height: character.height}});
 console.log(namesHeights);
+
 //4. Get array of all first names
 const firstNames = characters.map(function(character){ return character.name.split(" ")[0]});
 console.log(firstNames);
 
-//***REDUCE***
+//**************
+//*** REDUCE ***
+//**************
 //1. Get total mass of all characters
 var totalMass = characters.reduce(function(totalMass, character){ return totalMass + character.mass }, 0);
 console.log(totalMass);
+
 //2. Get total height of all characters
 var totalHeight = characters.reduce(function(totalHeight, character){ return totalHeight + character.height }, 0);
 console.log(totalHeight);
+
 //3. Get total number of characters by eye color
 var charactersEyeColors = characters.reduce(function(prev, character){ 
     if(character.eye_color in prev){
@@ -63,85 +72,73 @@ var charactersEyeColors = characters.reduce(function(prev, character){
     return prev;
 }, {});
 console.log(charactersEyeColors);
+
 //4. Get total number of characters in all the character names
 var totalNumberOfChars = characters.reduce(function(prev, cur){
     return prev + cur.name.length;
 }, 0);
 console.log(totalNumberOfChars);
 
-//***FILTER***
+//**************
+//*** FILTER ***
+//**************
 //1. Get characters with mass greater than 100
-const charsMassGreater100 = characters.filter(function(character){
-    return character.mass > 100;
-});
+const charsMassGreater100 = characters.filter(function(character){ return character.mass > 100; });
 console.log(charsMassGreater100);
+
 //2. Get characters with height less than 200
-const charsHeightLess200 = characters.filter(function(character){
-    return character.height < 200;
-});
+const charsHeightLess200 = characters.filter(function(character){ return character.height < 200; });
 console.log(charsHeightLess200);
+
 //3. Get all male characters
-const charactersMale = characters.filter(function(character){
-    return character.gender === 'male';
-});
+const charactersMale = characters.filter(function(character){ return character.gender === 'male'; });
 console.log(charactersMale);
+
 //4. Get all female characters
-const charactersFemale = characters.filter(function(character){
-    return character.gender === 'female';
-});
+const charactersFemale = characters.filter(function(character){ return character.gender === 'female'; });
 console.log(charactersFemale);
 
-//***SORT***
+//************
+//*** SORT ***
+//************
 //1. Sort by mass
-console.log(characters.sort(function(char1, char2){
-    return char1.mass > char2.mass
-}));
+console.log(characters.sort(function(char1, char2){ return char1.mass > char2.mass }));
+
 //2. Sort by height
-console.log(characters.sort(function(char1, char2){
-    return char1.height > char2.height
-}));
+console.log(characters.sort(function(char1, char2){ return char1.height > char2.height }));
+
 //3. Sort by name
-console.log(characters.sort(function(char1, char2){
-    return char1.name.toUpperCase() > char2.name.toUpperCase()
-}));
+console.log(characters.sort(function(char1, char2){ return char1.name.toUpperCase() > char2.name.toUpperCase() }));
+
 //4. Sort by gender
-console.log(characters.sort(function(char1, char2){
-    return char1.gender.toUpperCase() > char2.gender.toUpperCase()
-}));
+console.log(characters.sort(function(char1, char2){ return char1.gender.toUpperCase() > char2.gender.toUpperCase() }));
 
-
-//***EVERY***
+//*************
+//*** EVERY ***
+//*************
 //1. Does every character have blue eyes?
-console.log(characters.every(function(character){ 
-    return character.eye_color === 'blue';
-}));
-//2. Does every character have mass more than 40?
-console.log(characters.every(function(character){ 
-    return character.mass > 40;
-}));
-//3. Is every character shorter than 200?
-console.log(characters.every(function(character){ 
-    return character.height < 200;
-}));
-//4. Is every character male?
-console.log(characters.every(function(character){ 
-    return character.gender === 'male';
-}));
+console.log(characters.every(function(character){ return character.eye_color === 'blue'; }));
 
-//***SOME***
+//2. Does every character have mass more than 40?
+console.log(characters.every(function(character){ return character.mass > 40; }));
+
+//3. Is every character shorter than 200?
+console.log(characters.every(function(character){ return character.height < 200; }));
+
+//4. Is every character male?
+console.log(characters.every(function(character){ return character.gender === 'male'; }));
+
+//************
+//*** SOME ***
+//************
 //1. Is there at least one male character?
-console.log(characters.some(function(character){ 
-    return character.gender === 'male';
-}));
+console.log(characters.some(function(character){ return character.gender === 'male'; }));
+
 //2. Is there at least one character with blue eyes?
-console.log(characters.some(function(character){ 
-    return character.eye_color === 'blue';
-}));
+console.log(characters.some(function(character){ return character.eye_color === 'blue'; }));
+
 //3. Is there at least one character taller than 210?
-console.log(characters.some(function(character){ 
-    return character.height > 210;
-}));
+console.log(characters.some(function(character){ return character.height > 210; }));
+
 //4. Is there at least one character that has mass less than 50?
-console.log(characters.some(function(character){ 
-    return character.mass < 50;
-}));
+console.log(characters.some(function(character){ return character.mass < 50; }));
